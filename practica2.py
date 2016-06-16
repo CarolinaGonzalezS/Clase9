246451a114aca0f1c771e5879f9c2e7bbca317cd
CarolinaGonzalezS <carolay-96@live.com>
2016-06-16 07:29:10 -0500

    Primeros ejercicios

---
+++ practica2.py
@@ -0,0 +1,11 @@
+import time 
+#devuelve el tiempo actual como año, dias, meses,horas, mintos
+t=time.localtime()
+year=t[0]
+month=t[1]
+day=t[2]
+hora=t[3]
+print(year)
+print(month)
+print(day)
+print(hora)
