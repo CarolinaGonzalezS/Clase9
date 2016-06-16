807a809ee5716b192d950c7448a93b8de66f6b2f
CarolinaGonzalezS <carolay-96@live.com>
2016-06-16 07:40:02 -0500

    primeros ejercicios

--- practica1.py
+++ practica1.py
@@ -1,2 +1,12 @@
 import time 
 print(time.localtime())
+#devuelve el tiempo actual como año, dias, meses,horas, mintos
+t=time.localtime()
+year=t[0]
+month=t[1]
+day=t[2]
+hora=t[3]
+print(year)
+print(month)
+print(day)
+print(hora)
--- practica2.py
+++ practica2.py
@@ -1,11 +1,6 @@
-import time 
-#devuelve el tiempo actual como año, dias, meses,horas, mintos
-t=time.localtime()
-year=t[0]
-month=t[1]
-day=t[2]
-hora=t[3]
-print(year)
-print(month)
-print(day)
-print(hora)
+import time
+for x in range(1,61):
+    print(x)
+for x in range(1,61):
+    print(x)
+    time.sleep(1)   
